e61a2fef1d966444d78f98b3052d0372571f0c47
Vaspera <110002885+FromDawnToDay@users.noreply.github.com>
2023-01-29 17:32:15 -0700

    Create template_file.cpp

---
+++ template_file.cpp
@@ -0,0 +1,13 @@
+#include "vazLIB.h"
+#include <iostream>
+
+using namespace std;
+using namespace vazLIB;
+using namespace vazLIB::literals;
+using namespace vazLIB::literals::time;
+using namespace vazLIB::utils;
+using namespace vazLIB::data_types;
+
+int main() {
+	
+};
