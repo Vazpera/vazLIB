de076b11dafc611f89ebdda1e1cc39ae280c23d0
Vaspera <110002885+Vazpera@users.noreply.github.com>
2023-01-31 10:19:44 -0700

    Create vazLIB_base.h

---
+++ vazLIB_modules/vazLIB_base.h
@@ -0,0 +1,45 @@
+#include <vector>
+
+#ifndef vazLIB_base
+#define vazLIB_base
+
+namespace vazLIB 
+{
+
+	namespace utils 
+	{
+		/* Seperate a std::string */
+		std::vector<std::string> seperate(std::string str, char delim) {
+		  std::string word = "";
+		  std::vector<std::string> words;
+		  for (auto x : str) {
+		    if (x == delim) {
+		      words.push_back(word);
+		      word = "";
+		    } else {
+		      word += x;
+		    }
+		  }
+		  words.push_back(word);
+		  return words;
+		};
+		/* Seperate a char* */
+		std::vector<std::string> seperate(char *str, char delim) {
+			std::string s = str;
+		  std::string word = "";
+		  std::vector<std::string> words;
+		  for (auto x : s) {
+		    if (x == delim) {
+		      words.push_back(word);
+		      word = "";
+		    } else {
+		      word += x;
+		    }
+		  }
+		  words.push_back(word);
+		  return words;
+		};
+	}
+}
+
+#endif // vazLIB_base
